f60b3abe5ce5f96996734860cf5b2cee23dca860
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-11-06 21:06:48 +0000

    Buildable.

--- api/actions/resend.ts
+++ api/actions/resend.ts
@@ -1,7 +1,7 @@
 'use server'
 import {Resend} from 'resend';
 
-export async function resendTest(body: string) {
+export async function resendTest(body?: string) {
     const resend = new Resend(process.env.RESEND_API_KEY_TEST_MESSAGES);
 
     return await resend.emails.send({
--- app/api/messaging/route.ts
+++ app/api/messaging/route.ts
@@ -1,4 +1,4 @@
-import {resendTest} from "@/api/actions/resend.ts";
+import {resendTest} from "@/api/actions/resend";
 
 export async function POST(request: any) {
     await resendTest(JSON.stringify(request))
