9aa2487100d0529290de367ddc1695bd2dfcd97e
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-11-09 19:37:22 +0000

    Test endpoint to email me the payload of a webhook.

--- app/api/notify-matching/route.ts
+++ app/api/notify-matching/route.ts
@@ -1,3 +1,14 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { withAuthorizationHeader } from "@/app/api/notify-by-email/withAuthorizationHeader";
+import { resendNotification } from "@/api/actions/resend";
 
-export default async function POST(request: NextRequest) {}
+export async function POST(request: NextRequest) {
+  return withAuthorizationHeader(async () => {
+    const newVar = await request.json();
+    await resendNotification(
+      { content: JSON.stringify(newVar), subject: "testing" },
+      "willwritescode@gmail.com",
+    );
+    return new NextResponse();
+  });
+}
