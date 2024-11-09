378a60680e60bd6ca5f06b5581a4976912637b8a
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-11-09 19:42:12 +0000

    Removed console logs.

--- app/api/notify-by-email/withAuthorizationHeader.ts
+++ app/api/notify-by-email/withAuthorizationHeader.ts
@@ -3,11 +3,8 @@ import { headers } from "next/headers";
 
 export async function withAuthorizationHeader(handler: () => Promise<any>) {
   const headerObj = await headers();
-  console.log(headerObj);
   const authorization = headerObj.get("Authorization");
-  console.log(authorization);
   const apiKey = authorization?.split(" ")?.pop();
-  console.log(apiKey);
   if (!apiKey || apiKey !== process.env.NOTIFICATION_KEY) {
     return new Response(JSON.stringify({ message: "Not authorized" }), {
       status: 401,
